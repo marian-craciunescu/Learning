a7d31151281e1f15e06b1a5d1159291b3d081555
groot.patricia <patricia>
2015-05-22 01:31:23 +0300

    4 variabilelee

--- lesson1/helloworld.c
+++ lesson1/helloworld.c
@@ -10,6 +10,14 @@ int main ()
 	//print integer
 	printf("intreg=%i \n",intreg);
 	int intreg2=17;
-	printf("%i \n %i",intreg,intreg2);
+	printf("%i %i \n",intreg,intreg2);
+	//print only one character
+	char caractero='P';
+	printf("%c %i %c %i\n", 'F',24,caractero,5);
+	//print floating point number
+	printf("%lf\n",2.3);
+	//print a string
+	char *derp="le string";
+	printf("%s\n",derp);
 	return 0;
 }
