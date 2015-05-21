01c4af3e9797fcaecf65a514532ac2d337aa5d1e
groot.patricia <patricia>
2015-05-22 01:05:50 +0300

    add helloworld.c to master

---
+++ lesson1/helloworld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+//int main ( int arg_number,char[] *arguments)
+int main ()
+{
+	printf("hello world!\n");
+	int intreg;//declarare
+	intreg=16;//atribuire
+	//print integer
+	printf("intreg=%i \n",intreg);
+	int intreg2=17;
+	printf("%i \n %i",intreg,intreg2);
+	return 0;
+}
