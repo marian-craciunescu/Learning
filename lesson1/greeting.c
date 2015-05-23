b0ffb89d5b486103a7f26c6b3268483f1f74ec3c
groot.patricia <patricia>
2015-05-23 19:04:39 +0300

    Added some explanation for the tema1.c

--- lesson1/greeting.c
+++ lesson1/greeting.c
@@ -1,1 +1,15 @@
 
+#include <stdio.h>
+
+int main()
+{
+	//vom invata sa folosim o noua functie.Care primeste de la tastatura input.
+	//scanf(char * format,...)	;
+	//se aseamana f mult cu printf.Foloseste aceeasi specificatori.Stocheaza valoarea primita de la tastatura intr-o variabila.
+	//Ex:
+	int a;
+	printf("Introduceti un numar intreg =");
+	scanf("%i", &a); //observa te rog ca se foloseste operatorul & care inseamna "adresa de  " sau locul unde va fi stocata valoarea citita
+	printf("NUmarul introdus este =%i \n",a);
+ 	return 0;
+}
--- lesson1/tema1.c
+++ lesson1/tema1.c
@@ -6,30 +6,41 @@ int main()
 	
 //eu facusem prima oara asa:
 
-//int intreg1;
-//int intreg2;
-//int intreg3;
-//intreg1=2;
-//intreg2=4;
-//intreg3=6;
-//printf("%i %i %i \n",intreg1,intreg2,intreg3);
+int intreg1;
+int intreg2;
+int intreg3;
+intreg1=2;
+intreg2=4;
+intreg3=6;
+printf("%i %i %i \n",intreg1,intreg2,intreg3);
 
 //si mi-a printat - intreg12, intreg24, intreg36. hmmm.... ?
+// nu inteleg intrebarea...dupa cum poti sa vezi..printeaza la fel 2 4 6
 
+/*
 int intreg1=2;
 int intreg2=4;
 int intreg3=6;
+*/
 
 printf("%i %i %i \n",intreg1,intreg2,intreg3);
 
 //aici este string-ul
 
 char *stringuLuPatricia="GICU ARE MERE"; //nu ma lasa sa pun spatiu in nume o_O
+/**
+ * E normal sa nu te lase sa pui spatii in numele de variabile.
+ * */
 printf("%s \n",stringuLuPatricia);
 
 // aici este mix cu floating point - dar cum scap eu de cele 5 0?
+/***
+ * adaugand un nou identificator. 
+ *  %.2lf ii spune ca dupa virgula sa aiba fix 2 zecimale.Prin numarul dinaintea lui lf specifici cate zecimale vor fi reprezentate dupa virgula.
+ * 
+ * */
 
 char *stringuDeBaza="Scularie";
-printf("%i %lf %s \n",intreg1,2.4,stringuDeBaza);
+printf("%i %.2lf %s \n",intreg1,2.4,stringuDeBaza);
 return 0;
 }
